0d2a87dc9df383ddfe9443c0e1f1e068996cc738
Olivia Fratangelo <oliviaf7777@gmail.com>
2023-07-14 10:28:03 -0400

    setting up pokeBank.js file

--- app.js
+++ app.js
@@ -1,12 +1,57 @@
+const morgan = require("morgan");
 const express = require("express");
-
 const app = express();
+app.use(morgan("dev"));
+const pokeBank = require("./pokeBank");
 
-app.use(express.static("public"));
+app.get("/", (req, res) => {
+  const pokemonList = pokeBank.list();
+  let html = "<h1>Pokedex</h1>";
+  pokemonList.forEach((pokemon) => {
+    html += `<p><a href="/pokemon/${pokemon.id}">${pokemon.name}</a></p>`;
+  });
+  res.send(html);
+});
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/pokemon/:id", (req, res) => {
+  const pokemon = pokeBank.find(req.params.id);
+  if (!pokemon) {
+    res.status(404).send("Pokemon not found");
+  } else {
+    let html = `<!DOCTYPE html>
+    <html>
+      <head>
+        <title>My Pokedex</title>
+        <link rel="stylesheet" href="/style.css" />
+      </head>
+      <body>
+        <div class="pokemon-list">
+          <header><img src="/logo.png" />Pokedex</header>
+          ${pokemon
+            .map(
+              (pokemon) => `
+          <div class="pokemon-item">
+            <p>
+              <span class="pokemon-position">${pokemon.id}. ▲</span>${pokemon.name}
+              <small>(Trained by ${pokemon.trainer})</small>
+            </p>
+            <small class="pokemon-info">
+              Type: ${pokemon.type} | Date Caught: ${pokemon.date}
+            </small>
+          </div>
+          `
+            )
+            .join("")}
+        </div>
+      </body>
+    </html>
+    `;
+    res.send(html);
+  }
+});
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`server is listening on port ${PORT}`);
+  console.log(`App listening in port ${PORT}`);
 });
