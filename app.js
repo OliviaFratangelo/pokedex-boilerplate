b98454f323273b91eaf68278feba34c239ed943f
Olivia Fratangelo <oliviaf7777@gmail.com>
2023-07-14 11:01:15 -0400

    adding routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 app.use(morgan("dev"));
+const pokeBank = require("./pokeBank");
 
 app.get("/",(req,rest) => {
   const pokemonList = pokeBank.list();
@@ -12,7 +13,44 @@ app.get("/",(req,rest) => {
   rest.send(html);
 });
 
+app.get("/pokemon/:id",(req,res) => {
+  const pokemon = pokeBank.find(req.params.id);
+  const post = pokeBank.find(id);
+  if(!pokemon) {
+    res.status(404).send("Pokemon not found"); }
+    else {
+      res.send(`<!DOCTYPE html>
+      <html>
+        <head>
+          <title>My Pokedex</title>
+          <link rel="stylesheet" href="/style.css" />
+        </head>
+        <body>
+          <div class="pokemon-list">
+            <header><img src="/logo.png" />Pokedex</header>
+            ${pokemon
+              .map(
+                (pokemon) => `
+            <div class="pokemon-item">
+              <img class="pokemon-img" src={pokemon.image} />
+              <p>
+                <span class="pokemon-position">${pokemon.id}. ▲</span>${pokemon.name}
+                <small>(Trained by ${pokemon.trainer})</small>
+              </p>
+              <small class="pokemon-info">
+                Type: ${pokemon.type} | Date Caught: ${pokemon.date}
+              </small>
+            </div>`
+              )
+              .join("")} 
+          </div>
+        </body>
+      </html>
+      `); 
+    }
+  });
+
 const PORT = 3000;
-app.listen(Port, () => {
+app.listen(PORT, () => {
   console.log(`serving is listening on port ${PORT}`)
 });
